61a61405ff9fb84292ee9d64fce6ab6bb845d85a
Christopher Homberger <christopher.homberger@web.de>
2020-05-22 17:37:49 +0200

    Add links to failed to download from gplay

--- googleapkdownloadtask.cpp
+++ googleapkdownloadtask.cpp
@@ -25,7 +25,7 @@ void GoogleApkDownloadTask::start() {
     m_playApi->getApi()->delivery(m_packageName.toStdString(), m_versionCode, std::string())->call([this](playapi::proto::finsky::response::ResponseWrapper&& resp) {
         auto dd = resp.payload().deliveryresponse().appdeliverydata();
         if((dd.has_gzippeddownloadurl() ? dd.gzippeddownloadurl() : dd.downloadurl()) == "") {
-            throw std::runtime_error("To use the download feature, Minecraft: Bedrock Edition has to be purchased on the Google Play Store.\nIf you are trying to download a beta version, please make sure you are in the Minecraft beta program on Google Play and then try again after a while (joining the program might take a while).");
+            throw std::runtime_error("To use the download feature, <a href=\"https://play.google.com/store/apps/details?id=com.mojang.minecraftpe\">Minecraft: Bedrock Edition has to be purchased on the Google Play Store</a>.<br>If you are trying to download a beta version, please make sure you are in the <a href=\"https://play.google.com/apps/testing/com.mojang.minecraftpe\">Minecraft beta program on Google Play.</a> and then try again after a while (joining the program might take a while).");
         }
         startDownload(dd);
     }, [this](std::exception_ptr e) {
