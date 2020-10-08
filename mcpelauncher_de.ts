4e939a89dbb45394bb0126e35e80ad89a5197a52
Christopher Homberger <christopher.homberger@web.de>
2020-10-08 20:56:50 +0200

    Update localisation template

--- mcpelauncher_de.ts
+++ mcpelauncher_de.ts
@@ -6,7 +6,26 @@
     <message>
         <location filename="qml/EditProfileWindow.qml" line="46"/>
         <source>Edit profile</source>
-        <translation>Profil bearbeiten</translation>
+    </message>
+    <message>
+        <location filename="qml/EditProfileWindow.qml" line="52"/>
+        <source>Delete profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/EditProfileWindow.qml" line="124"/>
+        <source>Latest</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/EditProfileWindow.qml" line="124"/>
+        <source>compatible</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/EditProfileWindow.qml" line="130"/>
+        <source>installed</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -22,7 +41,46 @@
     <message>
         <location filename="qml/LauncherMain.qml" line="44"/>
         <source>Unofficial Linux Launcher</source>
-        <translation type="unfinished">Inoffizieller Linux und macOS Launcher</translation>
+    </message>
+    <message>
+        <location filename="qml/LauncherMain.qml" line="221"/>
+        <source>Open log</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/LauncherMain.qml" line="221"/>
+        <source>Please sign in again</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/LauncherMain.qml" line="221"/>
+        <source>Download and play</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/LauncherMain.qml" line="221"/>
+        <source>Sign in or import .apk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/LauncherMain.qml" line="221"/>
+        <source>Play</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/LauncherMain.qml" line="221"/>
+        <source>Unsupported Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/LauncherMain.qml" line="222"/>
+        <source>Game is running</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/LauncherMain.qml" line="222"/>
+        <source>Please wait...</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
