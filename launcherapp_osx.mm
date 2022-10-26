4f3ddffeee5ba14768f0b6221957819a1c77cc08
ChristopherHX <christopher.homberger@web.de>
2022-10-26 21:21:31 +0200

    Update launcherapp_osx.mm

--- launcherapp_osx.mm
+++ launcherapp_osx.mm
@@ -3,8 +3,4 @@
 #import <AppKit/AppKit.h>
 
 void LauncherApp::setVisibleInDock(bool visible) {
-    if (!visible)
-        [NSApp setActivationPolicy: NSApplicationActivationPolicyAccessory];
-    else
-        [NSApp setActivationPolicy: NSApplicationActivationPolicyRegular];
 }
