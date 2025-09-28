8ce569e114b80cae63a7edfa7a49d5b7e49dcdf3
ChristopherHX <christopher.homberger@web.de>
2025-09-28 22:22:15 +0200

    Change GLFW source URL and update FetchContent logic
    Updated GLFW URL and modified content fetching logic.

--- ext/glfw.cmake
+++ ext/glfw.cmake
@@ -8,12 +8,16 @@ set(GLFW_BUILD_WAYLAND OFF CACHE BOOL "" FORCE)
 
 FetchContent_Declare(
         glfw3_ext
-        URL "https://github.com/glfw/glfw/archive/master.zip"
+        URL "https://github.com/minecraft-linux/glfw/archive/3ec31853c4b3c2eb1c066dab56bf8443ae82f8dd.zip"
 )
 
-FetchContent_GetProperties(glfw3_ext)
-if(NOT glfw3_ext_POPULATED)
-  FetchContent_Populate(glfw3_ext)
-  add_subdirectory(${glfw3_ext_SOURCE_DIR} ${glfw3_ext_BINARY_DIR})
+
+if(POLICY CMP0169)
+  FetchContent_MakeAvailable(glfw3_ext)
+else()
+  if(NOT glfw3_ext_POPULATED)
+    FetchContent_Populate(glfw3_ext)
+    add_subdirectory(${glfw3_ext_SOURCE_DIR} ${glfw3_ext_BINARY_DIR})
+  endif()
 endif()
 add_library(glfw3 ALIAS glfw)
