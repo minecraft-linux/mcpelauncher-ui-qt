9396e6c8a2c8497d99e1900553464e1a036fab47
metah <hahupung@gmail.com>
2026-01-12 07:35:46 +0100

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 99.6% (283 of 284 strings)
    
    Translation: minecraft-linux/mcpelauncher-ui-qt
    Translate-URL: https://hosted.weblate.org/projects/minecraft-linux/mcpelauncher-ui-qt/pt_BR/

--- mcpelauncher_pt_BR.ts
+++ mcpelauncher_pt_BR.ts
@@ -454,7 +454,7 @@ Você pode testar este launcher gratuitamente, ativando o modo de teste.</transl
     <message>
         <location filename="qml/HomeScreen.qml" line="655"/>
         <source>Latest Minecraft Version %1 is a beta version, which is hidden by default.</source>
-        <translation>A versão mais recente do Minecraft %1 é uma versão beta, que é oculta por padrão.</translation>
+        <translation type="unfinished">A versão mais recente do Minecraft %1 é uma versão beta, que é oculta por padrão.</translation>
     </message>
     <message>
         <location filename="qml/HomeScreen.qml" line="679"/>
